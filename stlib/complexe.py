dfe6d1b8bbb4e0face67d70fba90f8148bd2bfdc
CeVague <cedric.vachaudez@netc.fr>
2023-07-03 22:23:26 +0200

    Update complexe.py
    Création du formulaire de base temporairement

--- stlib/complexe.py
+++ stlib/complexe.py
@@ -5,21 +5,38 @@ def run(dataset, client_line):
     import pandas as pd
     import plotly.express as px
     import matplotlib.pyplot as plt
-
-    @st.cache_data
-    def load_dataset(sample=None):
-        df = pd.read_pickle("C:/Users/Administrateur/Documents/OpenClassrooms/GitHub/Projet_7-Dashboard/data/streamlit_dataset.pkl")
-        if sample is None:
-            return df
-        else:
-            #return df.sample(sample)
-            return df.iloc[:sample]
-
     
-    mask_t0 = (dataset['TARGET'] == 0)
+    @st.cache_data
+    def get_mask():
+        return dataset['TARGET'] == 0
+    
+    mask_t0 = get_mask()
+    
+    list_cols = list(dataset.columns)
     
     # Affichage
     st.title("Rapport B")
+    
+    form = st.form("select_columns", clear_on_submit=False)
+    with form:
+        col1, col2 = st.columns(2)
+        with col1:
+            feat_1 = st.selectbox("Première colonne à visualiser", list_cols)
+        with col2:
+            feat_2 = st.selectbox("Deuxième colonne à visualiser", ['-----'] + list_cols)
+        inverse = st.checkbox("Inverser les axes ?")
+
+        # Now add a submit button to the form:
+        st.form_submit_button("Actualiser")
+    
+    # Affichage du graphique
+    fig, ax = plt.subplots()
+    if feat_2 not in list_cols:
+        dataset[feat_1].dropna().plot()
+    else:
+        dataset[[feat_1, feat_2]].dropna().plot()
+    st.pyplot()
+        
     
 # This code allows you to run the app standalone
 # as well as part of a library of apps
