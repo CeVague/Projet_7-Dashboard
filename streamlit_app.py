899339fa3aeb53be4ddad633e3e7cc5eeab0dfbd
CeVague <cedric.vachaudez@netc.fr>
2023-07-10 10:34:40 +0200

    Mise en forme
    Ajustement de l'ordre et ajout de commentaires

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,30 +16,34 @@ from stlib import edit_client, resume, personalised_graph
 
 import requests
 
+# ------------- Chargement de variables et de données ---------------
+
 # Chargement des variables en fonction de l'environnement
 if os.environ.get('ENVIRONMENT') == 'local':
     load_dotenv('config_local.env')
 else:
     load_dotenv('config_production.env')
 
+# Chargement des chemins utiles qui viennent d'être chargés
 API_DATAFRAME_URL = os.environ.get('API_DATAFRAME_URL')
 API_PLOT_URL = os.environ.get('API_PLOT_URL')
 API_PREDICT_URL = os.environ.get('API_PREDICT_URL')
 CLIENT_INFO_FILE = os.environ.get('CLIENT_INFO_FILE')
 DATASET = os.environ.get('DATASET')
 
-@st.cache_data
+# Chargement des infos clients (les quelques informations
+# données pour s'assurer que l'on parle du bon client)
 def load_data_info():
     df = pd.read_pickle(CLIENT_INFO_FILE)
     return df
 
-@st.cache_data
+# Chargement du dataset complet ou d'un extrait selon les besoins
 def load_dataset(sample=None):
     df = pd.read_pickle(DATASET)
     if sample is None:
         return df
     else:
-        #return df.sample(sample)
+        # Sample sans mélange pour connaitre les ID clients présentes
         return df.iloc[:sample]
 
 
@@ -47,23 +51,15 @@ def load_dataset(sample=None):
 # le dataset n'est accessible qu'en le donnant en
 # parametres aux fonctions. Or Streamlit le gère
 # mal et met longtemps à relancer get_client_line
+# Du coup pas besoin de mise en cache non plus
 
-#Mise en forme de la page
-st.set_page_config(page_title="Rapport d'analyse du statut client", page_icon='./img/logo.png', layout="wide")
+# Chargement des données de base
+infos_client = load_data_info()
 
 # Chargement du gros dataset
 dataset = load_dataset()
 
-# Chargement des données de base
-infos_client = load_data_info()
-    
-    
-@st.cache_data
-def get_client_line(sk_id):
-    index = dataset.SK_ID_CURR.eq(sk_id).argmax()
-    return dataset.iloc[index]
-
-
+# ---------- Interaction avec l'API -------------
 @st.cache_data
 def predict_client(client_line):
     reponse = requests.get(API_PREDICT_URL, json={'data': client_line.to_json(default_handler=str)})
@@ -107,22 +103,39 @@ def get_client_shap_plot(client_line, forme='waterfall'):
         st.error('Erreur lors de la récupération de l\'image')
         return None
 
-def show_client(df, sk_id, show):
+
+# -------- Fonctions utiles --------------
+
+# Récupère une ligne client d'après son SK_ID (c'est à dire
+# toutes les features et leurs valeurs connues d'un client)
+@st.cache_data
+def get_client_line(sk_id):
+    index = dataset.SK_ID_CURR.eq(sk_id).argmax()
+    return dataset.iloc[index]
+
+# Affichage des données d'un client
+def show_client(sk_id, show):
+    # S'il n'y a pas d'ID on renvoie None
     if sk_id=="":
         return None
     
+    # Si l'ID n'est pas un chiffre on prévient et renvoie None
     if not sk_id.isdigit():
         show.write('ID client doit être un entier')
         return None
     
+    # Sinon c'est un entier donc on le converti
     sk_id = int(sk_id)
     
-    if len(df.loc[df['SK_ID_CURR']==sk_id]) == 0:
+    # Si aucun client ne correspond on prévient et renvoie None
+    if len(infos_client.loc[infos_client['SK_ID_CURR']==sk_id]) == 0:
         show.write('ID client introuvable')
         return None
     
+    # Sinon on récupère les infos du client
     client_line = get_client_line(sk_id)
     
+    # Et on affiche ses infos
     genre = "Inconu"
     if client_line['CODE_GENDER']==0:
         genre = "Homme"
@@ -133,49 +146,45 @@ def show_client(df, sk_id, show):
     show.write('Genre : ' + genre)
     show.write('Type de prêt : ' + ('Comptant' if client_line['NAME_CONTRACT_TYPE']==0 else 'Renouvelable'))
     
+    # On affiche aussi le statut prédit par le modèle
     statut = predict_client(client_line)['result']
-    show.markdown("Statut : " + (":green[Accepté]"if client_line['TARGET']==0 else ':red[Refusé]'))
-    show.markdown("# Statut predit : " + (":green[Accepté]"if statut==0 else ':red[Refusé]'))
+    #show.markdown("Statut : " + (":green[Accepté]"if client_line['TARGET']==0 else ':red[Refusé]'))
+    show.markdown("# Statut prédit : " + (":green[Accepté]"if statut==0 else ':red[Refusé]'))
+    
+    # On renvoie l'ID pour signifier que tout s'est bien passé
     return sk_id
 
     
 def main():
-    #st.sidebar.write(os.environ.get('API_DATAFRAME_URL'))
-    #st.sidebar.write(os.environ.get('API_PLOT_URL'))
-    #st.sidebar.write(os.environ.get('CLIENT_INFO_FILE'))
-    #st.sidebar.write(os.environ.get('DATASET'))
+    #Mise en forme de la page
+    st.set_page_config(page_title="Rapport d'analyse du statut client", page_icon='./img/logo.png', layout="wide")
     
     # Création de la sidebar
-    col1, col2, col3= st.sidebar.columns([1, 2, 1])
+    # Avec le logo
+    col1, col2, col3= st.sidebar.columns([1, 6, 1])
     col1.write("")
     col2.image('./img/logo.png')
     col3.write("")
     
+    # La texte input et les infos clients récupérées
     st.sidebar.title("Infos client")
     
     sk_id = st.sidebar.text_input('ID Client', placeholder="Entrez l'ID client (exemple : 100002)")
     
-    sk_id = show_client(infos_client, sk_id, st.sidebar)
+    sk_id = show_client(sk_id, st.sidebar)
     
     st.sidebar.divider()
     
-    # Si l'id client a un soucis on stop tout là
+    # Si l'id client a un soucis on stop tout ici
     if sk_id is None:
         st.stop()
-    
-    
     
     # Chargement de la ligne du client
     client_line = get_client_line(sk_id)
     
-    
-    
     # Récupération des données Shap
     shap_df = get_client_shap(client_line)
     shap_img = get_client_shap_plot(client_line)
-    
-    
-    
     
     # Chargement des autres pages sous forme de modules
     pages = {'resume':resume, 'edit_client':edit_client, 'personalised_graph':personalised_graph}
@@ -183,9 +192,8 @@ def main():
     # Affiche le menu de sélection de page
     with st.sidebar:
         page = st.selectbox("Mode d'analyse:", pages.keys(), format_func=lambda k:pages[k].description)
-        #st.caption("Choisissez entre un résumé du choix de l'algorythme, ou une version où vous pouvez modifier les graphiques et tester d'autres paramètres pour les données client")
     
-    # Lance la page sélectionnée
+    # Lance la page sélectionnée en lui passant les données chargées
     pages[page].run(dataset, client_line, shap_df, shap_img)
     
 
